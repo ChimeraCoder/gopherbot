d9ee9b8100f03f6477360387b72d93d3c9ea6b27
jimt <jimteeuwen@gmail.com>
2012-10-09 17:39:48 +0200

    Adds testdata/makecert.sh script. It can be used to create a test certificate and key for a TLS connection.

---
+++ testdata/makecert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Call this script with an email address (valid or not),
+# to create a test certificate and key for use with a TLS connection.
+
+if [ -z $1 ]; then
+	echo "Usage: makecert.sh joe@random.com";
+	exit 1;
+fi
+
+mkdir -p certs;
+rm certs/*;
+
+openssl req -new -nodes -x509 -out certs/cert.pem -keyout certs/key.pem -days 3650 \
+	-subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=$1";
+
+exit $?;
